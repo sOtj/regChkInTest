4f0f0f50aa62c15c5829484633dce8b15b6c2263
Suzuki <suzukiyumiotjiwarongo@gmail.com>
2025-08-18 20:59:38 +0200

    form1-chk sheetName

--- testReDirect.js
+++ testReDirect.js
@@ -32,13 +32,13 @@ function runProcedureBasedOnTime() {
 
     // Define time ranges in minutes since midnight
     const timeAStart = 7 * 60 + 30;     // 07:30
-    const timeAEnd = 12 * 60 + 30;      // 12:30
+    const timeAEnd = 22 * 60 + 30;      // 12:30
 
-    const timeBStart = 12 * 60 + 40;    // 12:40
-    const timeBEnd  = 13 * 60 + 40;     // 13:40
+    const timeBStart = 22 * 60 + 40;    // 12:40
+    const timeBEnd  = 22 * 60 + 45;     // 13:40
 
-    const timeCStart = 13 * 60 + 50;    // 13:50
-    const timeCEnd  = 22 * 60 + 50;     // 16:50  ***22:50 FOR TEST***
+    const timeCStart = 22 * 60 + 50;    // 13:50
+    const timeCEnd  = 22 * 60 + 55;     // 16:50  ***22:50 FOR TEST***
 
     // Check which time range the current time falls into
     // **CRITICAL FIX**: Replaced single = with triple === for comparison.
